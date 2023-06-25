d993d2eca261ee9e0ebabb819494bd2817811136
FilipeGPaganoto <123755494+FilipeGPaganoto@users.noreply.github.com>
2023-06-25 10:18:10 -0300

    ExerciseConditionals01

---
+++ ExerciseConditionals01.java
@@ -0,0 +1,17 @@
+public class ExerciseConditionals01 {
+    public static void main(String[] args) {
+        double salary = 11000;
+        double result;
+        String percentageValue;
+
+        if (salary > 5000) {
+            result = (salary * 30) / 100;
+            percentageValue = "30% = " + result;
+        } else {
+            result = (salary * 15) / 100;
+            percentageValue = "15% = " + result;
+        }
+
+        System.out.println(percentageValue);
+    }
+}
