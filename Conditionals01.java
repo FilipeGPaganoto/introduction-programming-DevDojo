b551d06071875d8cbb816d254d8f9a52d5edd1ad
FilipeGPaganoto <123755494+FilipeGPaganoto@users.noreply.github.com>
2023-06-25 09:29:49 -0300

    Conditionals01

--- Conditionals01.java
+++ Conditionals01.java
@@ -2,9 +2,11 @@ public class Conditionals01 {
     public static void main(String[] args) {
         boolean condition = true;
 
-        if (condition == true) {
+        if (condition) {
             System.out.println("Inside IF statement");
+        } else {
+            System.out.println("Inside ELSE");
         }
-        System.out.println("Outside If statement");
+        System.out.println("Outside IF-ELSE");
     }
 }
