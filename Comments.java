d2d43f408d06f8f18859c0b5a66a55afa7f66a62
FilipeGPaganoto <123755494+FilipeGPaganoto@users.noreply.github.com>
2023-06-25 09:20:57 -0300

    HelloWorld

---
+++ Comments.java
@@ -0,0 +1,13 @@
+/*
+ This is a comment
+ did you understand?
+ */
+public class Comments {
+    public static void main(String[] args) {
+        // This is a menssage that you can write anything you want !*$!
+        String firstName = "Luffy";
+        // This is the last name == WRONG!
+        String lastName = "Monkey D.";
+        System.out.println(firstName + " " + lastName);
+    }
+}
