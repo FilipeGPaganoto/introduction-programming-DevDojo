a11155f7ee667f5b07a986d55589d6303f3abfa6
FilipeGPaganoto <123755494+FilipeGPaganoto@users.noreply.github.com>
2023-06-25 10:17:59 -0300

    Conditionals03

---
+++ Conditionals03.java
@@ -0,0 +1,14 @@
+public class Conditionals03 {
+    public static void main(String[] args) {
+        int firstAge = 20;
+        int secondAge = 11;
+
+        // boolean isFirstAgeGreaterOrEqualThanSecondAge = firstAge >= secondAge;
+
+        if ( firstAge >= secondAge) {
+            System.out.println("The first age is greater or equal second age");
+        } else {
+            System.out.println("The first age is not greater or equal second age");
+        }
+    }
+}
