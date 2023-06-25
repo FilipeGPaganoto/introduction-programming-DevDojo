574b98f878c33797828df791dfa8677ba5da7bbf
FilipeGPaganoto <123755494+FilipeGPaganoto@users.noreply.github.com>
2023-06-25 09:24:33 -0300

    ExerciseAgeDifference

---
+++ ExerciseAgeDifference.java
@@ -0,0 +1,8 @@
+public class ExerciseAgeDifference {
+    public static void main(String[] args) {
+        int firstAge = 40, secondAge = 10;
+        int agrDifference = firstAge - secondAge;
+
+        System.out.println("The difference between the first age: " + firstAge + " and the second age: "+ secondAge +" is " + agrDifference);
+    }
+}
