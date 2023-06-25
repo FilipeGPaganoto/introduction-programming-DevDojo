2bcc921b6f28eb38ccb985ad3f63b453c54edadc
FilipeGPaganoto <123755494+FilipeGPaganoto@users.noreply.github.com>
2023-06-25 09:22:04 -0300

    Variables02

---
+++ Variables02.java
@@ -0,0 +1,20 @@
+public class Variables02 {
+    public static void main(String[] args) {
+        byte age = 17;
+        short houseNumber = 1000;
+        int counter = 2000000000;
+        long serialNumber = 239249320;
+        float regularSalary = 5000.5F;
+        double elonMuskFortune = 12200.0;
+        boolean condition = false;
+        char gender = 'M';
+        System.out.println("Your age is: " + age);
+        System.out.println("Your house number is: " + houseNumber);
+        System.out.println("Your counter is: " + counter);
+        System.out.println("Your serial number is: " + serialNumber);
+        System.out.println("Your regular salary is: " + regularSalary);
+        System.out.println("Your Elon Musk Fortune is: " + elonMuskFortune);
+        System.out.println("Your condition is: " + condition);
+        System.out.println("Your gender is: " + gender);
+    }
+}
