c1f775088597faa2e13478059939d11811464346
FilipeGPaganoto <123755494+FilipeGPaganoto@users.noreply.github.com>
2023-06-25 11:19:49 -0300

    Conditionals05

---
+++ Conditionals05.java
@@ -0,0 +1,17 @@
+public class Conditionals05 {
+    public static void main(String[] args) {
+        String savedName = "Aatrox";
+        String savedCreditCardNumber = "123";
+        String savedSecurityCode = "303";
+
+        String inputName = "Aatrox";
+        String inputCreditCardNumber = "123";
+        String inputSecurityCode = "303";
+
+        if (savedName.equals(inputName) && savedCreditCardNumber.equals(inputCreditCardNumber) && savedSecurityCode.equals(inputSecurityCode)) {
+            System.out.println("Can buy!");
+        } else {
+            System.out.println("Can't buy!");
+        }
+    }
+}
