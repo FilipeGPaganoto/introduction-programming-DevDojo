a80a9d6eae530598707ec9e7f4a7d8960f60e5ca
FilipeGPaganoto <123755494+FilipeGPaganoto@users.noreply.github.com>
2023-06-25 09:22:11 -0300

    Variables03

---
+++ Variables03.java
@@ -0,0 +1,7 @@
+public class Variables03 {
+    public static void main(String[] args) {
+        String name = "Filipe";
+        int age = 17;
+        System.out.println("Name: " + name + " and your age is: " + age);
+    }
+}
