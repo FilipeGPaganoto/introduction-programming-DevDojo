cf3aad8cdc2098a846124e9e7b525f41569cecdf
FilipeGPaganoto <123755494+FilipeGPaganoto@users.noreply.github.com>
2023-06-28 17:45:47 -0300

    While01

---
+++ academy/devdojo/loops/whiles/While01.java
@@ -0,0 +1,13 @@
+package academy.devdojo.loops.whiles;
+
+public class While01 {
+    public static void main(String[] args) {
+        byte counter = 0;
+
+        while (counter < 10) {
+            System.out.println(counter + "HEEEEEEEEEY FILIIIIIIIPE!" + counter);
+            counter++;
+        }
+
+    }
+}
